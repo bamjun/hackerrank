240534cc49069a98cceaa57e3aac77810183b754
bamjun <khy5116@naver.com>
2022-03-03 16:28:21 +0900

    Create Counting Sort 1.py

---
+++ Counting Sort 1.py
@@ -0,0 +1,42 @@
+# Prepare > Interview Preparation Kits > 1 Week Preparation Kit > Day 2 > Counting Sort 1
+# https://www.hackerrank.com/challenges/one-week-preparation-kit-countingsort1/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-two&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    
+    result_list = [0]*len(arr)
+
+    for x in range(len(arr)):
+        result_list[arr[x]] = result_list[arr[x]] + 1
+        
+    
+    return result_list[:100]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
